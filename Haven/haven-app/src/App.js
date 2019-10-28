e84a22d627588e6204a4ddf519629ecb3c550bfe
Miles Grossenbacher <mrg498@nyu.edu>
2019-10-28 17:13:11 -0400

    Updated Navbar for user story #39

--- Haven/haven-app/src/App.js
+++ Haven/haven-app/src/App.js
@@ -4,28 +4,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/navbar.component";
-import UserList from "./components/user-list.component";
-import CreateUser from "./components/create-user.component";
-import FindUser from "./components/find-user.component";
-import UpdateUser from "./components/update-user.component";
-import DeleteUser from "./components/delete-user.component";
+//import CreateUser from "./components/create-user.component";
+//import FindUser from "./components/find-user.component";
+//import UpdateUser from "./components/update-user.component";
+//import DeleteUser from "./components/delete-user.component";
 
 function App() {
   return (
-    <Router>
-    <div className="App">
-    Welcome to the Haven.
+
+    <div>
+      <Navbar/>
     </div>
-    <div className="container">
-      <Navbar />
-      <br/>
-      <Route path="/" exact component={UserList} />
-      <Route path="/add" component={CreateUser} />
-      <Route path="/:id" component={FindUser} />
-      <Route path="/update/:id" component={UpdateUser} />
-      <Route path="/delete" component={DeleteUser} />
-    </div>
-    </Router>
   );
 }
 
