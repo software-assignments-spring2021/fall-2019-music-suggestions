ce1f196bd145361663d951afbd676fbb99d6c2fe
Miles Grossenbacher <mrg498@nyu.edu>
2019-11-12 16:54:26 -0500

    connected welcome page to main page

---
+++ Haven/haven-app/src/components/browse-artists.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Browse extends Component {
+  render() {
+    return (
+      <div>
+        <p>Browse Artists</p>
+      </div>
+    )
+  }
+}
---
+++ Haven/haven-app/src/components/browse-music.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class BrowseMusic extends Component {
+  render() {
+    return (
+      <div>
+        <p>Browse Music</p>
+      </div>
+    )
+  }
+}
