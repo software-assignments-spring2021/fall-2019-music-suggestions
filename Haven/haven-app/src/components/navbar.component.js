e9c920a6e0c30c34dd38490b4fc916bfc987ac67
soospitz <sks568@nyu.edu>
2019-11-17 16:40:52 -0500

    demo

--- Haven/haven-app/src/components/navbar.component.js
+++ Haven/haven-app/src/components/navbar.component.js
@@ -7,17 +7,17 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/browse" className="navbar-brand">Haven Logo</Link>
+        <Link to="/browse" className="navbar-brand">Haven</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
           <Link to="/createUser" className="nav-link">Sign Up</Link>
           </li>
           <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Exercise Log</Link>
+          <Link to="/create" className="nav-link">Something</Link>
           </li>
           <li className="navbar-item">
-          <Link to="/user" className="nav-link">Create User</Link>
+          <Link to="/user" className="nav-link">Create Profile</Link>
           </li>
         </ul>
         </div>
