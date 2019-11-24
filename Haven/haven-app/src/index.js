11301b1a5ab1c05cc1dabfd44ce507a44dd64b30
Jimmy Lin <jwl400@nyu.edu>
2019-11-23 22:11:19 -0500

    Set up retrieve information from MongoDB

--- Haven/haven-app/src/index.js
+++ Haven/haven-app/src/index.js
@@ -13,44 +13,9 @@ const routing = (
     <Route exact path = "/" component = {WelcomePage} />
     <Route path="/signin" component = {SignIn}/>
     <Route path="/browse" component = {App}/>
+    <Route path="/all" component = {FindUser}/>
   </Router>
 )
 
 
-// import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(routing, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-
-
-//
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import './index.css'
-// import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-// import App from './App'
-// import Users from './users'
-// import Contact from './contact'
-// const routing = (
-//   <Router>
-//     <div>
-//       <ul>
-//         <li>
-//           <Link to="/">Home</Link>
-//         </li>
-//         <li>
-//           <Link to="/users">Users</Link>
-//         </li>
-//         </ul>
-//       <Route exact path="/" component={App} />
-//       <Route path="/users" component={Users} />
-//       <Route path="/contact" component={Contact} />
-//     </div>
-//   </Router>
-// )
-// ReactDOM.render(routing, document.getElementById('root'))
