9e265a930f1b0bd555aab2dc84034f4d8b36f751
leenaloo <ll3337@nyu.edu>
2019-12-04 17:30:14 -0500

    changed logo routing and also fixed welcome page css

--- Haven/haven-app/src/components/create-user.component.js
+++ Haven/haven-app/src/components/create-user.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import "../css/create-user.css"
+import "../css/create-user.css";
 
 class NewUserFactory {
   constructor (username, userType){
