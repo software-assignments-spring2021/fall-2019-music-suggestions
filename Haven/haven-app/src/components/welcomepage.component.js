135c647ea9444a56737f4a7619b96570c11241f4
leenaloo <ll3337@nyu.edu>
2019-12-14 16:19:03 -0500

    fixed welcome page

--- Haven/haven-app/src/components/welcomepage.component.js
+++ Haven/haven-app/src/components/welcomepage.component.js
@@ -16,16 +16,8 @@ export default class UserList extends Component {
           <p className="HavenText-Welcome">Welcome to the Haven.<br />
             Haven is your place to <br /> find local artists!<br />
             Get started now.</p><br />
-          <br />
 
-            <button className="button" onclick="window.location.href = '#section2';">
-          See More</button>
-          </div>
-        <div className="main" id="section2">
-          <h2 className="HavenText-Welcome"> Whether you are looking to <br /> hire a photographer for an event, <br />
-            or you are trying to join a band, <br />
-            Haven is the platform for you <br />to find and collaborate<br />
-            with artists in your area.</h2>
+
           <Link to= "/browse">
             <button className="button" type = "button">
               Explore Artists
