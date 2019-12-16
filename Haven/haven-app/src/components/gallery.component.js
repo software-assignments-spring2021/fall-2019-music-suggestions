e0b0e4afa2472ce4a0bf04198c2981c7378c7329
soospitz <sks568@nyu.edu>
2019-12-16 16:20:22 -0500

    fix buttons and adding

--- Haven/haven-app/src/components/gallery.component.js
+++ Haven/haven-app/src/components/gallery.component.js
@@ -40,7 +40,9 @@ const Profile = props => (
               <a type="button" className="btn-floating btn-small btn-dribbble"><i className="fab fa-dribbble" /></a></span>
             </div>
             <div id="button">
-              <button type="button" class="btn btn-outline-primary waves-effect" Link to={"/profiles/"+props.profile._id}>View Profile</button>
+              <button type="button" class="btn btn-outline-primary waves-effect">
+              <Link to={"/profiles/"+props.profile._id}>View Profile</Link>
+              </button>
             </div>
         </div>
 
