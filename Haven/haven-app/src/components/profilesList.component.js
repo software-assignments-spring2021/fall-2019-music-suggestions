aae09e5a257bdff3a148681d6f56b00ee75cb0ae
Jimmy Lin <jwl400@nyu.edu>
2019-12-17 20:27:16 -0500

    Implemented routing for My Profiles

--- Haven/haven-app/src/components/profilesList.component.js
+++ Haven/haven-app/src/components/profilesList.component.js
@@ -7,12 +7,58 @@ import "../css/myprofile.css"
 import * as jwt_decode from 'jwt-decode';
 
 const Profile = props => (
-  <tr>
-    <td>{props.profile.user_id}</td>
-    <td>{props.profile.user_type}</td>
-    <td>{props.profile.display_name}</td>
-    <td>{props.profile.genre}</td>
-  </tr>
+
+<div className="card-deck" style={{margin: '10px'}}>
+  <div>
+    {/* Card */}
+    <div className="card mb-4" id="card">
+      {/* Card image */}
+      <div className="view view-cascade overlay">
+        <div id="crop">
+          <img className="card-img-top" id="img" src={"http://localhost:5000/" + props.profile.profileImage1} alt="Card image cap" />
+        </div>
+        <a><div className="mask rgba-white-slight"/></a></div>
+
+        <div className="card-body card-body-cascade text-center" id="cardContent">
+          {/* Title */}
+
+            <h4 className="card-title"><strong>{props.profile.display_name}</strong></h4>
+
+          {/* Subtitle */}
+          <div id="cardSubtitle">
+            <h7><strong>{props.profile.user_type}</strong></h7>
+          </div>
+          {/* Text */}
+
+            <div id="cardText">{props.profile.genre} </div>
+
+            <div id="social">
+              {/* Facebook */}
+              <span>
+              <a type="button" className="btn-floating btn-small btn-fb"><i className="fab fa-facebook-f" /></a></span>
+              {/* Twitter */}
+              <span>
+              <a type="button" className="btn-floating btn-small btn-tw"><i className="fab fa-twitter" /></a></span>
+              {/* Google + */}
+              <span>
+              <a type="button" className="btn-floating btn-small btn-dribbble"><i className="fab fa-dribbble" /></a></span>
+            </div>
+            <div id="button">
+              <button type="button" class="btn btn-outline-primary waves-effect">
+              <Link to={"/profiles/"+props.profile._id}>View Profile</Link>
+              </button>
+            </div>
+
+
+        </div>
+
+        <div className="card-footer text-muted text-center">
+        {props.profile.location}
+        </div>
+
+      </div>
+    </div>
+</div>
 )
 
 
@@ -48,7 +94,7 @@ export default class profilesList extends Component {
 
   render() {
     return (
-      <div>
+      <div className="card-deck" style={{margin: '10px'}}>
         {this.ProfileList()}
       </div>
     )
